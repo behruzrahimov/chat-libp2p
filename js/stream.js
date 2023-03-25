1fcaf55c382997b0caf9e4e46a2a6a3aa7ca830f
behruzrahimov <behruzrahimov580@gmail.com>
2023-03-25 14:48:43 +0500

    fix stream message

--- js/stream.js
+++ js/stream.js
@@ -1,23 +1,43 @@
+/* eslint-disable no-console */
+
 import { pipe } from "it-pipe";
 import * as lp from "it-length-prefixed";
 import map from "it-map";
+
+function uint8ArrayToString(buf) {
+  return new TextDecoder().decode(buf);
+}
+function uint8ArrayFromString(str) {
+  return new TextEncoder().encode(str);
+}
 export function stdinToStream(stream) {
+  // Read utf-8 from stdin
   process.stdin.setEncoding("utf8");
   pipe(
+    // Read from stdin (the source)
     process.stdin,
-    (source) => map(source, (string) => string),
+    // Turn strings into buffers
+    (source) => map(source, (string) => uint8ArrayFromString(string)),
+    // Encode with length prefix (so receiving side knows how much data is coming)
     lp.encode(),
+    // Write to the stream (the sink)
     stream.sink
   );
 }
 
 export function streamToConsole(stream) {
   pipe(
+    // Read from the stream (the source)
     stream.source,
+    // Decode length-prefixed data
     lp.decode(),
-    (source) => map(source, (buf) => buf.subarray()),
+    // Turn buffers into strings
+    (source) => map(source, (buf) => uint8ArrayToString(buf.subarray())),
+    // Sink function
     async function (source) {
+      // For each chunk of data
       for await (const msg of source) {
+        // Output the data as a utf8 string
         console.log("> " + msg.toString().replace("\n", ""));
       }
     }
